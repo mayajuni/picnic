8c3f1c31a42a3dd68b6e790d14a35deeb07dfe57
동준 <mayajuni@naver.com>
2015-06-19 12:12:41 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by 동준 on 2015-04-22.
+ */
+var express = require('express');
+var http = require("http");
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+/** 모듈 및 레디스 설정 등등 */
+/*app.use(favicon(dirName + '/public/dist/imgs/favicon.ico'));*/
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/** static 경로 */
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join('../upload')));
+
+/** 라우터 설정 */
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/public/dist/index.html");
+});
+
+/** 서버구동 */
+http.createServer(app).listen(80, function(){
+  console.log("Express server listening on port " + 80);
+});
+
+module.exports = app;
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+/**
+ * Created by 동준 on 2015-05-03.
+ */
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    imagemin = require('gulp-imagemin'),
+    del = require('del'),
+    templateCache = require('gulp-angular-templatecache'),
+    minifyHTML = require('gulp-minify-html');
+
+/* console.log 같은거 제거해주는거 */
+var stripDebug = require('gulp-strip-debug');
+
+// Styles
+gulp.task('styles', function() {
+    return gulp.src([
+        'public/src/css/*.css'
+    ])
+        .pipe(concatCss('app.css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/dist/css'));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+    return gulp.src('public/src/js/**/*.js')
+        .pipe(concat('app.js'))
+        /*.pipe(stripDebug())*/
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/dist/js'));
+});
+
+// 이미지 압축
+gulp.task('images', function() {
+    return gulp.src('public/src/img/**/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('public/dist/img'));
+});
+
+// html 변환
+gulp.task('angular-template', function(){
+    return gulp.src(['public/src/templates/**/*.html'])
+        .pipe(templateCache())
+        .pipe(gulp.dest('public/dist/templates'));
+});
+
+// index.html 파일
+gulp.task('index-min', function(){
+    return gulp.src('public/src/index.html')
+        .pipe(minifyHTML())
+        .pipe(gulp.dest('public/dist'))
+});
+
+// Clean
+gulp.task('clean', function(cb) {
+    del(['public/dist/css', 'public/dist/js', 'public/dist/imgs', 'public/dist/templates'], cb)
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images', 'angular-template', 'index-min');
+});
+
+// Watch
+gulp.task('watch', function() {
+    // Watch .scss files
+    gulp.watch('public/src/css/*.css', ['styles']);
+
+    // Watch .js files
+    gulp.watch(['public/src/js/**/*.js'], ['scripts']);
+
+    // Watch image files
+    gulp.watch('public/src/img/**/*', ['images']);
+
+    // Watch index files
+    gulp.watch('public/src/index.html', ['index-min']);
+
+    // Watch html files
+    gulp.watch('public/src/templates/**/*.html', ['angular-template']);
+});
